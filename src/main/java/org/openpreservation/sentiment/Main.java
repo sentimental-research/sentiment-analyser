c5436c31bbc2979463f1b822ea66f0a9353d8a02
Gary Macindoe <garymacindoe@users.noreply.github.com>
2016-03-23 15:18:40 +0000

    Check for null or empty tweet before cleanup

--- src/main/java/org/openpreservation/sentiment/Main.java
+++ src/main/java/org/openpreservation/sentiment/Main.java
@@ -86,11 +86,11 @@ public final class Main {
 					continue;
 				// Extract the tweet text (3rd field)
 				String tweet = csvLine[TWEET_TEXT];
+				if ((tweet == null) || tweet.isEmpty())
+					continue;
 
 				// Clean up the tweet
 				tweet = cleanup(tweet);
-				if ((tweet == null) || tweet.isEmpty())
-					continue;
 
 				// Calculate the score for the tweet
 				int score = SentimentAnalyser.calculateSentiment(tweet);
