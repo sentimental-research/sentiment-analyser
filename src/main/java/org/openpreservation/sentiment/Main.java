a42db71a7e544282d8bab22c37ecab78717d5307
Carl Wilson <carl@openpreservation.org>
2016-03-23 13:14:42 +0000

    Re-factored to get tests working

--- src/main/java/org/openpreservation/sentiment/Main.java
+++ src/main/java/org/openpreservation/sentiment/Main.java
@@ -3,9 +3,19 @@
  */
 package org.openpreservation.sentiment;
 
-import java.io.*;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -15,118 +25,97 @@ import com.opencsv.CSVWriter;
  */
 public final class Main {
 
-  /**
-   * Index of tweeter in CSV row.
-   */
-  public static final int TWEET_USER = 0;
+	/**
+	 * Index of tweeter in CSV row.
+	 */
+	public static final int TWEET_USER = 0;
 
-  /**
-   * Index of date in CSV row.
-   */
-  public static final int TWEET_DATE = 1;
+	/**
+	 * Index of date in CSV row.
+	 */
+	public static final int TWEET_DATE = 1;
 
-  /**
-   * Index of tweet content in CSV row.
-   */
-  public static final int TWEET_TEXT = 2;
+	/**
+	 * Index of tweet content in CSV row.
+	 */
+	public static final int TWEET_TEXT = 2;
 
-  /**
-   * Index of tweet ID in CSV row.
-   */
-  public static final int TWEET_ID   = 3;
+	/**
+	 * Index of tweet ID in CSV row.
+	 */
+	public static final int TWEET_ID = 3;
 
-  public static void main(String[] args) {
-    // First argument is input CSV file (optional - default is to eat CSV from
-    // stdin)
-    // Second argument is output CSV file (optional - default is to poo CSV
-    // to stdout)
-    if (args.length == 0 || args.length > 2) {
-      System.err.println("Usage: annotate.jar [input.csv] [output.csv]");
-      System.exit(1);
-    }
+	public static void main(String[] args) {
+		// First argument is input CSV file (optional - default is to eat CSV
+		// from
+		// stdin)
+		// Second argument is output CSV file (optional - default is to poo CSV
+		// to stdout)
+		if (args.length > 2) {
+			System.err.println("Usage: annotate.jar [input.csv] [output.csv]");
+			System.exit(1);
+		}
 
-    // Create reader from filename in args[0] or stdin if args is empty
-    Reader input = null;
-    try {
-      input = new BufferedReader((args.length > 0) ? new FileReader(args[0])
-                                        : new InputStreamReader(System.in));
-    } catch (FileNotFoundException e) {
-      System.out.println(e.toString());
-      System.exit(1);
-    } catch (IOException e) {
-      System.out.println(e.toString());
-      System.exit(1);
-    }
+		// Create reader from filename in args[0] or stdin if args is empty
+		// Use ';' as field separator, '"' as field quote character, skip
+		// first
+		// line
+		// as header
+		try (Reader reader = new BufferedReader(
+				(args.length > 0) ? new FileReader(args[0]) : new InputStreamReader(System.in));
+				Writer writer = new BufferedWriter(
+						(args.length > 1) ? new FileWriter(args[1]) : new OutputStreamWriter(System.out))) {
+			annotateCsv(reader, writer);
+		} catch (IOException e) {
+			System.out.println(e.toString());
+			System.exit(1);
+		}
 
-    // Create writer from filename in args[1] or stdout if args.length <= 1
-    Writer output = null;
-    try {
-      output = new BufferedWriter((args.length > 1) ? new FileWriter(args[1])
-                                        : new OutputStreamWriter(System.out));
-    } catch (IOException e) {
-      System.out.println(e.toString());
-      System.exit(1);
-    } finally {
-      try {
-        input.close();
-      } catch (IOException e) {
-        System.out.println(e.toString());
-        System.exit(1);
-      }
-    }
+		System.exit(0);
+	}
 
-    // Use ';' as field separator, '"' as field quote character, skip first line
-    // as header
-    CSVReader reader = new CSVReader(input, ';', '"', 1);
-    CSVWriter writer = new CSVWriter(output, ';', '"');
+	public static void annotateCsv(final Reader input, final Writer output) throws IOException {
+		// Use ';' as field separator, '"' as field quote character
+		// skip first line as header
+		try (CSVReader reader = new CSVReader(input, ';', '"', 1); CSVWriter writer = new CSVWriter(output, ';', '"')) {
+			// Loop over all the tweets in the file
+			String[] tweet;
+			while ((tweet = reader.readNext()) != null) {
+				// Skip malformed lines
+				if (tweet.length < 3)
+					continue;
+				// Extract the tweet text (3rd field)
+				String text = tweet[TWEET_TEXT];
 
-    // Loop over all the tweets in the file
-    String[] tweet;
-    try {
-      while ((tweet = reader.readNext()) != null) {
-        // Extract the tweet text (3rd field)
-        String text = tweet[TWEET_TEXT];
+				// Clean up the tweet
+				text = cleanup(text);
 
-        // Clean up the tweet
-        text = cleanup(text);
+				// Calculate the score for the tweet
+				int score = SentimentAnalyser.calculateSentiment(text);
 
-        // Calculate the score for the tweet
-        int score = SentimentAnalyser.calculateSentiment(text);
+				// Append the score to the CSV row
+				List<String> tweetList = new ArrayList<>(Arrays.asList(tweet));
+				tweetList.add(Integer.toString(score));
 
-        // Append the score to the CSV row
-        List<String> tweetList = Arrays.asList(tweet);
-        tweetList.add(Integer.toString(score));
+				// Write out the line annotated with the score
+				writer.writeNext(tweetList.toArray(new String[0]));
+			}
+		}
+	}
 
-        // Write out the line annotated with the score
-        writer.writeNext((String[])tweetList.toArray());
-      }
-    } catch (IOException e) {
-      System.out.println(e.toString());
-      System.exit(1);
-    } finally {
-      try {
-        reader.close();
-        writer.close();
-      } catch (IOException e) {
-        System.out.println(e.toString());
-        System.exit(1);
-      }
-    }
-
-    System.exit(0);
-  }
-
-  /**
-   * Removes @usernames, #hashtags and http(s)://urls from a string using
-   * regular expressions.
-   *
-   * @param text  the input string
-   *
-   * @return the input string with @usernames, #hashtags and http(s)://urls
-   * removed.
-   */
-  public static String cleanup(String text) {
-    return text;  // TODO - use String.replaceAll(String regex).replaceAll()...
-  }
+	/**
+	 * Removes @usernames, #hashtags and http(s)://urls from a string using
+	 * regular expressions.
+	 *
+	 * @param text
+	 *            the input string
+	 *
+	 * @return the input string with @usernames, #hashtags and http(s)://urls
+	 *         removed.
+	 */
+	public static String cleanup(String text) {
+		return text; // TODO - use String.replaceAll(String
+						// regex).replaceAll()...
+	}
 
 }
--- src/test/java/org/openpreservation/sentitment/MainTest.java
+++ src/test/java/org/openpreservation/sentitment/MainTest.java
@@ -3,12 +3,22 @@
  */
 package org.openpreservation.sentitment;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 
 import org.junit.Test;
 import org.openpreservation.sentiment.Main;
 
-import java.io.*;
 import com.opencsv.CSVReader;
 
 /**
@@ -16,32 +26,30 @@ import com.opencsv.CSVReader;
  */
 public class MainTest {
 
-  /**
-   * Test method for
-   * {@link org.openpreservation.sentiment.Main#main(java.lang.String[])}
-   * .
-   */
-  @Test
-  public void testMain() throws IOException, FileNotFoundException {
-    ClassLoader classLoader = getClass().getClassLoader();
-    File input = new File(classLoader.getResource("output_got.csv").getFile());
-    File output = new File(input.getParent() + File.separator + "output_test.csv");
+	/**
+	 * Test method for
+	 * {@link org.openpreservation.sentiment.Main#main(java.lang.String[])} .
+	 */
+	@Test
+	public void testMain() throws IOException, FileNotFoundException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File input = new File(classLoader.getResource("output_got.csv").getFile());
+		File output = new File(input.getParent() + File.separator + "output_test.csv");
+		assertFalse(output.exists());
 
-    String[] args = { input.getAbsolutePath(), output.getAbsolutePath() };
+		Reader reader = new BufferedReader(new FileReader(input));
+		Writer writer = new BufferedWriter(new FileWriter(output));
 
-    assert(!output.exists());
+		Main.annotateCsv(reader, writer);
+		System.out.println("finished");
+		assertTrue(output.exists());
 
-    Main.main(args);
+		try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(output)))) {
 
-    assert(output.exists());
+			String[] line;
+			while ((line = csvReader.readNext()) != null)
+				assertTrue(line.length == 5);
 
-    CSVReader reader = new CSVReader(new BufferedReader(new FileReader(output)));
-
-    String[] line;
-    while ((line = reader.readNext()) != null)
-      assert(line.length == 5);
-
-    reader.close();
-  }
+		}
+	}
 }
-
